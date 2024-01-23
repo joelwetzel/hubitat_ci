a6e5b1f2258aea8b3ae40aeb501abbb0f97a8555
Joel Wetzel <jwetzel@affirma.com>
2024-01-22 18:35:07 -0600

    Makes the TimeKeeper into an Observable.  This is an intermediate step in providing an implementation of the BaseScheduler portion of AppExecutor.

--- src/main/groovy/me/biocomp/hubitat_ci/util/TimeKeeper.groovy
+++ src/main/groovy/me/biocomp/hubitat_ci/util/TimeKeeper.groovy
@@ -1,5 +1,7 @@
 package me.biocomp.hubitat_ci.util
 
+import java.util.concurrent.CopyOnWriteArrayList
+
 import groovy.time.*
 
 /**
@@ -11,12 +13,16 @@ import groovy.time.*
 class TimeKeeper {
     private Date internalDate = null
 
+    private final CopyOnWriteArrayList<TimeChangedListener> timeChangedListeners
+
     TimeKeeper() {
         internalDate = new Date()
+        timeChangedListeners = new CopyOnWriteArrayList<TimeChangedListener>()
     }
 
     TimeKeeper(Date startingDate) {
         internalDate = startingDate
+        timeChangedListeners = new CopyOnWriteArrayList<TimeChangedListener>()
     }
 
     /**
@@ -43,18 +49,56 @@ class TimeKeeper {
     }
 
     def advanceSeconds(int seconds) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, 0, 0, seconds, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
 
     def advanceMinutes(int minutes) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, 0, minutes, 0, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
 
     def advanceHours(int hours) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(0, hours, 0, 0, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
 
     def advanceDays(int days) {
+        def oldDate = internalDate
         internalDate = groovy.time.TimeCategory.plus(internalDate, new groovy.time.TimeDuration(days, 0, 0, 0, 0))
+        fireTimeChangedEvent(oldDate, internalDate)
     }
+
+    def addListener(TimeChangedListener listener) {
+        timeChangedListeners.add(listener)
+    }
+
+    def removeListener(TimeChangedListener listener) {
+        timeChangedListeners.remove(listener)
+    }
+
+    def fireTimeChangedEvent(Date oldTime, Date newTime) {
+        TimeChangedEvent event = new TimeChangedEvent(this, oldTime, newTime)
+        timeChangedListeners.each { listener ->
+            listener.timeChangedEventReceived(event)
+        }
+    }
+}
+
+class TimeChangedEvent extends EventObject {
+    Date oldTime
+    Date newTime
+
+    TimeChangedEvent(Object source, Date oldTime, Date newTime) {
+        super(source)
+        this.oldTime = oldTime
+        this.newTime = newTime
+    }
+}
+
+interface TimeChangedListener {
+    void timeChangedEventReceived(TimeChangedEvent event)
 }
